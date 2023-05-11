0835c1f95e74652709287e927fd4985c5b1cb25e
Farhan Fansuri <muhfarhan912391@gmail.com>
2023-05-11 15:37:36 +0700

    [MONGODB DAY 2 is Not Finished yet]

--- mongodb-day2.js
+++ mongodb-day2.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const Books = require('./books-schema')
 const port = 3000
 
 //connect
-mongoose.connect('mongodb://localhost:27017/books', ()=>{
-    console.log("connected")
-}, (e)=>{
+mongoose.connect('mongodb://127.0.0.1:27017/',{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("mongodb is connected")
+})
+.catch((e)=>{
     console.error(e)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/api/books/view',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
 })
